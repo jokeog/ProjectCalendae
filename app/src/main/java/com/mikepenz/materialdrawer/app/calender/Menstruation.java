139da09eb0058be9d4b41e429d5e497bc86b16ba
jokeog <joke.og@gmail.com>
2016-01-31 14:43:09 +0700

    เริ่มโปรเจค

---
+++ app/src/main/java/com/mikepenz/materialdrawer/app/calender/Menstruation.java
@@ -0,0 +1,201 @@
+package com.mikepenz.materialdrawer.app.calender;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.app.R;
+import com.mikepenz.materialdrawer.app.utils.CalendarFont;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.mikepenz.materialdrawer.app.decorators.EventDecorator;
+import com.mikepenz.materialdrawer.app.decorators.MySelectorDecorator;
+import com.mikepenz.materialdrawer.app.decorators.HighlightWeekendsDecorator;
+import com.mikepenz.materialdrawer.app.decorators.OneDayDecorator;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class Menstruation extends AppCompatActivity {
+
+    private Drawer result = null;
+
+    @Bind(R.id.calendarView)
+    MaterialCalendarView widget;
+
+    private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menstruation);
+        setTitle(R.string.drawer_item_Menstr_header);
+
+
+        int value[] = {R.id.msButtonS,R.id.msButtonE,R.id.textView2,R.id.msEditText,R.id.msTextView};
+
+        CalendarFont font =new CalendarFont() ;
+        font.setFonts(value, this) ;
+
+        ButterKnife.bind(this);
+
+        int color = -65536;//red
+        widget.setArrowColor(color);
+        widget.setSelectionColor(color);
+
+        widget.addDecorators(
+                new MySelectorDecorator(this),
+                new HighlightWeekendsDecorator(),
+                oneDayDecorator
+        );
+
+       new ApiSimulator().executeOnExecutor(Executors.newSingleThreadExecutor());
+
+
+
+
+
+
+
+
+
+
+        // Handle Toolbar
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withTranslucentStatusBar(false)
+                .withActionBarDrawerToggle(false)
+                .withSavedInstance(savedInstanceState)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem instanceof Nameable) {
+                            Toast.makeText(Menstruation.this, ((Nameable) drawerItem).getName().getText(Menstruation.this), Toast.LENGTH_SHORT).show();
+                        }
+
+                        return false;
+                    }
+                }).build();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
+    }
+
+
+    @OnClick (R.id.msButtonS)
+    void clickButtonS()
+    {
+        Button bSDate=(Button)findViewById(R.id.msButtonS);;
+        bSDate.setEnabled(false);
+        int color = -16711936;//green
+        widget.setArrowColor(color);
+        widget.setSelectionColor(color);
+        widget.clearSelection();
+    }
+
+    @OnClick (R.id.msButtonE)
+    void clickButtonE()
+    {
+        Button bEDate=(Button)findViewById(R.id.msButtonE);
+        bEDate.setEnabled(false);
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //add the values which need to be saved from the drawer to the bundle
+        outState = result.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+//
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private class ApiSimulator extends AsyncTask<Void, Void, List<CalendarDay>> {
+
+        @Override
+        protected List<CalendarDay> doInBackground(@NonNull Void... voids) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.MONTH, -2);
+            ArrayList<CalendarDay> dates = new ArrayList<>();
+            for (int i = 0; i < 30; i++) {
+                CalendarDay day = CalendarDay.from(calendar);
+                dates.add(day);
+                calendar.add(Calendar.DATE, 5);
+            }
+
+            return dates;
+        }
+
+        @Override
+        protected void onPostExecute(@NonNull List<CalendarDay> calendarDays) {
+            super.onPostExecute(calendarDays);
+
+            if (isFinishing()) {
+                return;
+            }
+
+            widget.addDecorator(new EventDecorator(Color.RED, calendarDays));
+        }
+    }
+
+
+}
